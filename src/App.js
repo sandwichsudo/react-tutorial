9f14b768127e9fea2c30b64acdd27e45ecbfd205
Gilly Ames <gilly.ames@octopus.energy>
2018-04-02 16:47:41 +0100

    Display some data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './Data';
 
 class App extends Component {
   render() {
+    const game = this.props.data[0];
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          Hello World
-        </p>
+        <div>
+          <h2>{game.name}</h2>
+          <h3>{game.minPlayers} - {game.maxPlayers} players</h3>
+          <img alt={game.name} src={game.image} width="200"/>
+        </div>
       </div>
     );
   }
+}
+
+App.defaultProps = {
+    data: data,
 }
 
 export default App;
